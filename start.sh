413409a16214d79b0e1b076bd146e6b421a25fad
Nick Esselman <nick.esselman@gmail.com>
2025-07-08 16:04:02 -0700

    :crying noises:

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 echo "[RUN] Starting headless server…"
-  ./build/server/server.x86_64 --headless --main-pack ./client/server/server.pck
+  ./build/server/server.x86_64 --headless --main-pack ./build/server/server.pck
