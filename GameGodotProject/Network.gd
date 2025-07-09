6f71c4de6699749bb236b7283044503d3e3e6d3c
Nick Esselman <nick.esselman@gmail.com>
2025-07-09 12:55:50 -0700

    (>_<)

--- GameGodotProject/Network.gd
+++ GameGodotProject/Network.gd
@@ -1,31 +1,36 @@
 extends Node
-
 signal game_started
 
 const PORT_NUMBER := 18818
 const MAX_PEERS := 16
 
-var peer := ENetMultiplayerPeer.new()
-var player_info: Dictionary = {}
-var lobbies: Dictionary = {}      # lobby_code → [peer_id]
+var multiplayer_peer: MultiplayerPeer
 var is_server := false
 var is_connected := false
-
-var player_id: int = 0
+var player_id := -1
+var player_info: Dictionary = {}  # keep track of joined players
 
 func _ready():
 	_connect_signals()
+
 	if OS.has_feature("server"):
+		# SERVER
 		is_server = true
 		player_id = 0
-		peer.create_server(PORT_NUMBER, MAX_PEERS)
-		multiplayer.multiplayer_peer = peer
+		multiplayer_peer = ENetMultiplayerPeer.new()
+		multiplayer_peer.create_server(PORT_NUMBER, MAX_PEERS)
 		print("Server running on port %d" % PORT_NUMBER)
 	else:
-		var server_address := "nick.hackclub.app"
-		peer.create_client(server_address, PORT_NUMBER)
-		multiplayer.multiplayer_peer = peer
-		print("Client connecting to %s:%d…" % [server_address, PORT_NUMBER])
+		# CLIENT (HTML5 → WebSocket; desktop → ENet)
+		if OS.has_feature("html5"):
+			multiplayer_peer = WebSocketMultiplayerPeer.new()
+			await multiplayer_peer.create_client("ws://nick.hackclub.app:%d" % PORT_NUMBER)
+		else:
+			multiplayer_peer = ENetMultiplayerPeer.new()
+			multiplayer_peer.create_client("nick.hackclub.app", PORT_NUMBER)
+		print("Client connecting to %s:%d…" % ["nick.hackclub.app", PORT_NUMBER])
+
+	multiplayer.multiplayer_peer = multiplayer_peer
 
 func _connect_signals():
 	multiplayer.peer_connected.connect(_on_player_connected)
@@ -34,42 +39,17 @@ func _connect_signals():
 	multiplayer.connection_failed.connect(_on_connected_fail)
 	multiplayer.server_disconnected.connect(_on_server_disconnected)
 
-#── SERVER RPC: join or create lobby by code ──#
-@rpc("authority")
-func join_lobby(code: String) -> void:
-	var pid = multiplayer.get_remote_sender_id()
-	if not lobbies.has(code):
-		lobbies[code] = []
-	if pid not in lobbies[code]:
-		lobbies[code].append(pid)
-	_broadcast_lobby_update(code)
-
-func _broadcast_lobby_update(code: String) -> void:
-	var members = lobbies[code]
-	for pid in members:
-		rpc_id(pid, "update_lobby_list", code, members)
-
-#── CLIENT RPC: receive updated member list ──#
-@rpc("any_peer")
-func update_lobby_list(code: String, members: Array) -> void:
-	print("Lobby '%s' now has peers: %s" % [code, members])
-
-#── CONNECTION SIGNALS ──#
 func _on_player_connected(pid: int) -> void:
 	if is_server:
-		# server‐side registration
+		# server-side registration
 		player_info[pid] = {"name": "Player_%d" % pid}
 		rpc_id(pid, "register_player", player_info[pid])
 		print("Player %d joined; sent registration" % pid)
-
 		# broadcast a "player joined" notice to everyone
 		rpc("player_joined", pid)
 
 func _on_player_disconnected(pid: int) -> void:
-	for code in lobbies.keys():
-		if pid in lobbies[code]:
-			lobbies[code].erase(pid)
-			_broadcast_lobby_update(code)
+	# simply remove from our registry and notify
 	player_info.erase(pid)
 	print("Player %d disconnected" % pid)
 	rpc("player_left", pid)
